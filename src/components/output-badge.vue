9470ace0a632acf9c36f8b14e3331c515f6f16a0
chrisroselli <croselli@contractornation.com>
2023-02-10 16:01:06 -0500

    updated logo

--- src/components/output-badge.vue
+++ src/components/output-badge.vue
@@ -7,7 +7,7 @@
             <td valign="top" style=
             "padding-top: 0; padding-bottom: 0; padding-left: 0; padding-right: 7px; border-top: 0; border-bottom: 0: border-left: 0;">
             <a href="https://www.contractornationlive.com/"><img width="175"
-                    height="77" style="width: 175px; max-width: 175px !175important; height: 77px; max-height: 77px !important;" id="preview-image-url" src="https://cdn.treehouseinternetgroup.com/cms_images/2535/CNLive_2023_sign_badge.jpg"
+                    height="77" style="width: 175px; max-width: 175px !175important; height: 77px; max-height: 77px !important;" id="preview-image-url" src="https://cdn.treehouseinternetgroup.com/cms_images/2535/CNLive_2023_sign-badge.jpg"
               name="preview-image-url" /></a>
                 <tr>
                   <td colspan="2" style="padding: 5px 41px;">
